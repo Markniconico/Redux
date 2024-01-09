c6ea5ce4cf8946038f19609969a8d1e27ed6b97b
喝水 <1720375829@qq.com>
2024-01-09 17:59:09 +0800

    实现中间件

--- redux/createStore.js
+++ redux/createStore.js
@@ -5,9 +5,21 @@ import actionTypes from './utils/actionTypes.js';
  * 实现createStore的功能
  * @param {function} reducer reducer
  * @param {any} defaultState 默认状态值
+ * @param {any} enhanced middleware返回的函数
  * @returns
  */
-export default function createStore(reducer, defaultState) {
+export default function createStore(reducer, defaultState, enhanced) {
+    if (typeof defaultState === 'function') {
+        //第二个参数是应用中间件的函数返回值
+        enhanced = defaultState;
+        defaultState = undefined;
+    }
+
+    if (typeof enhanced === 'function') {
+        //进入applyMiddleware的处理逻辑
+        return enhanced(createStore)(reducer, defaultState);
+    }
+
     let currentReducer = reducer, //当前使用的reducer
         currentState = defaultState; //当前的状态值
     const listeners = [];
